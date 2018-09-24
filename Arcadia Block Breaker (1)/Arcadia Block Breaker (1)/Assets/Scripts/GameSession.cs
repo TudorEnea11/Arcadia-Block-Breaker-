afd91623507387d242119686044fcf0514daf192
Tudor Enea <tudor_e@hotmail.com>
2018-09-24 23:48:52 +0200

    Pause Menu working as intended.

---
+++ Arcadia Block Breaker (1)/Arcadia Block Breaker (1)/Assets/Scripts/GameSession.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+
+    // Configuration Parameters
+    [Range(0.1f, 5f)] [SerializeField] public float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 69;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+
+    // States
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Time.timeScale = gameSpeed;
+	}
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+
+}
